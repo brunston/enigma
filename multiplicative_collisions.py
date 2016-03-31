1200434d28510e40bfcbb12246ebdd989f216a2e
Brunston Poon <brunston.io@outlook.com>
2016-03-31 10:03:22 -0400

    found colllisions for various key len in multiplicative cipher

---
+++ multiplicative_collisions.py
@@ -0,0 +1,9 @@
+#attempting to find key collisions for multiplicative
+
+k_length = input("k length?")
+k_length = int(k_length)
+n = input("n?")
+n = int(n)
+
+for i in range(26):
+    print("(i*n) mod k: {0}".format((i*n)%k_length))
