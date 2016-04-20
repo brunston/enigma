1f5a8e2e29131490b474ceb760b6d098112302e4
Brunston Poon <albatrossian@outlook.com>
2016-04-20 09:49:12 -0400

    added line to find_inverse, notes

--- find_inverse.py
+++ find_inverse.py
@@ -3,13 +3,14 @@
 
 z = int(input("z (int)? "))
 a = int(input("a (int)? "))
-
+inverse = -1
 for i in range(z):
     potential_inverse = (a*i) % z
 
     if (potential_inverse)==1:
+        inverse = i
         print("gcd is 1. Inverse: {0}".format(i))
     else:
         print("Not inverse. a*i in Z: {0}".format(potential_inverse))
 
-
+print("inverse (if -1 doesn't exist): {0}".format(inverse))
