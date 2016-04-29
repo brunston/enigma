45314d2ef9dbcddb73265fe8a6fbd57ff889af03
Brunston Poon <albatrossian@outlook.com>
2016-04-29 09:26:59 -0400

    RSA64 beginnings...

--- 593.py
+++ 593.py
@@ -1,7 +1,20 @@
 #class versions of all the other files
+import sys
+import random
+import math
 
 class NumberSet:
     "common base class for all number manipulations"
 
     def __init__(self, x=-1, z=-1, a=-1):
+        pass
+
+class RSA64:
+    """generates RSA keys and decrypts/encrypts up to keys of 64 bits
+       not cryptographically secure, also a textbook implementation"""
+
+    def __init__(self, key_size):
+        if key_size > 64:
+            sys.exit("key_size too large. try with bit < 64")            
+        self.key_size = key_size
         
